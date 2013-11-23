5abcc650ee34245904a73585bce67661c3ab6890
Chris Eichelberger <cne1x@ccri.com>
2013-11-23 11:09:49 -0500

    NGramDateTime initial tests pass.

---
+++ src/test/scala/org/eichelberger/ngram/NGramDateTimeTest.scala
@@ -0,0 +1,31 @@
+package org.eichelberger.ngram
+
+import org.specs2.mutable._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.joda.time.{DateTimeZone, DateTime}
+
+@RunWith(classOf[JUnitRunner])
+class NGramDateTimeTest extends Specification {
+  implicit val obj = NGramDateTime
+  val windowSize = 5
+
+  "empty ngram" should {
+    "be non-null" in {
+      val ngram = NGram[DateTime,String](windowSize)
+      ngram must not beNull
+    }
+  }
+
+  "single entry" should {
+    "be perfectly recoverable" in {
+      val dtIn = new DateTime().withZone(DateTimeZone.forID("UTC"))
+
+      val ngram = NGram.fromWhole(dtIn, windowSize)
+      ngram must not beNull
+
+      val dtOut = ngram.sample
+      dtOut must be equalTo dtIn
+    }
+  }
+}
